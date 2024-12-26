e50d63ae306e8bafd48a49cde4b4374b10193b35
Omar Ayman Gaber <149304486+Ammoor@users.noreply.github.com>
2024-12-26 09:17:35 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int ans = -1;
+        vector<short> fr(26);
+        for (int i = 0; i < s.size(); i++)
+            fr[s[i] - 'a']++;
+        for (int i = 0; i < s.size(); i++) {
+            if (fr[s[i] - 'a'] == 1) {
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+};
